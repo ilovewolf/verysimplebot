7d457d23af97c75fb4139cb3e9eeb61bca666a7b
ilovewolf <39267590+ilovewolf@users.noreply.github.com>
2018-07-30 10:23:45 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const self = new Discord.Client();
+
+self.on("ready", () => {
+  console.log("Sunt online 24/7 acum.");
+  self.user.setActivity(`inactiv.`);
+});
+
+self.login(process.env.TOKEN);
